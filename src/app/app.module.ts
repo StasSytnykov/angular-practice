6a7f2e7b7ac21ac05ef31434bdebea7078681704
StasSytnykov <stas82kak05@gmail.com>
2025-04-10 19:24:07 +0300

    Continue studying angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BooksComponent } from './books/books.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [AppComponent, BooksComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, FormsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
--- src/app/books/books.component.ts
+++ src/app/books/books.component.ts
@@ -1,11 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
+type Book = {
+  name: string;
+  author: string;
+  src: string;
+};
+
 @Component({
   selector: 'app-books',
   templateUrl: 'books.component.html',
   styleUrls: ['books.component.css'],
 })
 export class BooksComponent implements OnInit {
+  books: Book[] = [
+    {
+      name: 'Clean Code',
+      author: 'Robert C. Martin',
+      src: 'https://m.media-amazon.com/images/I/51E2055ZGUL._SY522_.jpg',
+    },
+    {
+      name: 'Clean Code 2',
+      author: 'Robert C. Martin 2',
+      src: 'https://m.media-amazon.com/images/I/51E2055ZGUL._SY522_.jpg',
+    },
+  ];
+
+  isShowing = false;
+  myText = '';
+
   constructor() {}
 
   ngOnInit(): void {}
